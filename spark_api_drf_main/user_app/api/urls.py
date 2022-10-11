6adee8303743bfdceb01c2059f554e46c069b3ad
rems <duplan.remi@gmail.com>
2022-10-10 21:00:38 -0400

    essayant d'utiliser un User custom de celui de rest_framework

--- spark_api_drf_main/user_app/api/urls.py
+++ spark_api_drf_main/user_app/api/urls.py
@@ -1,7 +1,10 @@
 from unicodedata import name
-from rest_framework.authtoken.views import obtain_auth_token
 from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
+
+from user_app.api.views import regitration_view
 
 urlpatterns = [
         path('login/', obtain_auth_token, name='login'),
+        path('register/', regitration_view, name='register'),
 ]
