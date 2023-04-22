32422eb05a89d564be92f0b1cf35986c7f6f5fcb
remi <duplanremi@gmail.com>
2023-04-22 00:56:28 -0400

    permission et authentification

--- spark_api_drf_main/user_app/api/views.py
+++ spark_api_drf_main/user_app/api/views.py
@@ -4,9 +4,14 @@ from django.contrib.auth.models import User
 from user_app.api.serializers import UserSerializer
 from django.contrib.auth import get_user_model
 from user_app.models import CustomUser
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 class UserViewSet(viewsets.ModelViewSet):
     User = get_user_model()
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
--- spark_api_drf_main/user_app/models.py
+++ spark_api_drf_main/user_app/models.py
@@ -1,16 +1,27 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 # Create your models here.
 
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('Email can not be null.')        
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
 class CustomUser(AbstractUser):
         
-        firstname = models.CharField(max_length=255, blank=False)
+        username = None
+        email = models.EmailField(verbose_name='email address', unique=True) 
         lastname = models.CharField(max_length=255, blank=False)
         description = models.TextField(max_length=255)
         password = models.CharField(max_length=255, blank=False)
         age = models.IntegerField(null=True, blank=True)
-        email = models.EmailField()
 
         profilPicture = models.ImageField(blank=True)
         headerPagePicture = models.ImageField(blank=True)
@@ -18,6 +29,9 @@ class CustomUser(AbstractUser):
         dateCreation = models.DateTimeField(auto_now_add=True, null=False)
         dateMiseAJour = models.DateTimeField(auto_now=True, null=False)
 
+        objects = CustomUserManager()
+        USERNAME_FIELD = 'email'
+        REQUIRED_FIELDS = []
 
         def __str__(self):
                 return self.firstname + " " + self.lastname
