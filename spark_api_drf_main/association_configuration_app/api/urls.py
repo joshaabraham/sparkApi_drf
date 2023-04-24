2ce9929a79680c54b08868de63872797634da13b
remi <duplanremi@gmail.com>
2023-04-23 22:42:28 -0400

    ajouté association config app

--- spark_api_drf_main/association_configuration_app/api/urls.py
+++ spark_api_drf_main/association_configuration_app/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from association_configuration_app.api.views import views
+from association_configuration_app.api.views import SportsAssociationConfigurationListCreateView, SportsAssociationConfigurationRetrieveUpdateDestroyView
 
 urlpatterns = [
-    path("sports_association_configurations/", views.SportsAssociationConfigurationListCreateView.as_view(), name="sports_association_configuration_list_create"),
-    path("sports_association_configurations/<int:pk>/", views.SportsAssociationConfigurationRetrieveUpdateDestroyView.as_view(), name="sports_association_configuration_detail"),
+    path("sports_association_configurations/", SportsAssociationConfigurationListCreateView.as_view(), name="sports_association_configuration_list_create"),
+    path("sports_association_configurations/<int:pk>/", SportsAssociationConfigurationRetrieveUpdateDestroyView.as_view(), name="sports_association_configuration_detail"),
 ]
--- spark_api_drf_main/association_configuration_app/api/views.py
+++ spark_api_drf_main/association_configuration_app/api/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics
 from association_configuration_app.models import SportsAssociationConfiguration
-from .serializers import SportsAssociationConfigurationSerializer
+from association_configuration_app.api.serializers import SportsAssociationConfigurationSerializer
 
 class SportsAssociationConfigurationListCreateView(generics.ListCreateAPIView):
     queryset = SportsAssociationConfiguration.objects.all()
