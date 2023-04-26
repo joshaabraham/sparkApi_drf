17ba6539111ed3c72df057cafbef6ce91817543f
remi <duplanremi@gmail.com>
2023-04-26 16:45:36 -0400

    changed shit

--- spark_api_drf_main/services_app/services/emailService/emailKey.py
+++
@@ -1,2 +0,0 @@
-EMAIL_HOST_USER=your_email@example.com
-EMAIL_HOST_PASSWORD=your_email_password
--- spark_api_drf_main/services_app/services/emailService/service_test.py
+++
@@ -1,7 +0,0 @@
-from emailService import send_email
-
-subject = "Test Email"
-body = "This is a test email from Python."
-to = "recipient@example.com"
-
-send_email(subject, body, to)
