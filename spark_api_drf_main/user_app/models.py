0b75b7d4c354bb22a8431d23275e1065c4edfe15
remi <duplanremi@gmail.com>
2023-04-26 23:59:12 -0400

    comment in user_app

--- spark_api_drf_main/user_app/models.py
+++ spark_api_drf_main/user_app/models.py
@@ -43,3 +43,55 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+    
+    
+    #     Informations personnelles de base:
+    #     Identifiant utilisateur (ID)
+    #     Nom complet
+    #     Date de naissance
+    #     Genre
+    #     Adresse e-mail
+    #     Numéro de téléphone
+    #     Photo de profil
+    #     Localisation géographique
+
+    # Informations de connexion:
+    #     Date de création du compte
+    #     Dernière connexion
+    #     Historique des connexions (IP, navigateur, appareil)
+
+    # Informations professionnelles et éducatives:
+    #     Emploi actuel
+    #     Entreprises précédentes
+    #     Secteur d'activité
+    #     Formation (écoles, universités, diplômes)
+
+    # Relations et interactions:
+    #     Liste d'amis / abonnés / abonnements
+    #     Groupes et communautés
+    #     Interactions avec les publications (likes, commentaires, partages)
+    #     Messages privés
+    #     Mentions et tags
+
+    # Préférences et centres d'intérêt:
+    #     Pages et comptes suivis
+    #     Centres d'intérêt (musique, films, sports, etc.)
+    #     Hashtags et sujets favoris
+    #     Langues et préférences de contenu
+
+    # Publications et contenu partagé:
+    #     Textes, images, vidéos, liens
+    #     Dates et heures de publication
+    #     Engagements et statistiques (nombre de vues, likes, commentaires)
+
+    # Paramètres de confidentialité et de sécurité:
+    #     Visibilité du profil
+    #     Permissions d'accès aux informations
+    #     Paramètres de notification
+    #     Historique des actions de modération (blocages, signalements, etc.)
+
+    # Données comportementales et d'engagement:
+    #     Temps passé sur la plateforme
+    #     Types de contenu consommés
+    #     Fréquence et durée des sessions
+    #     Actions et réactions aux publicités
